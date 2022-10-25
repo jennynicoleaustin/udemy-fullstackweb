462c18e101207fb4f2805f14bb75a4970f4cfa2f
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-25 11:30:40 -0500

    classlist

---
+++ Section 24: Intro to DOM/classList.js
@@ -0,0 +1,12 @@
+// classList
+
+// select the element
+
+const h2 = document.querySelector('h2')
+
+// methods built in to class list.
+h2.classList.add()
+
+//toggle classes with classList
+
+h2.classList.toggle('purple')
