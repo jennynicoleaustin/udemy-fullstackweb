85dc36d4f8f1684f2a16a80f339f9e25adef26ae
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-25 16:00:55 -0500

    clarify differences between __proto__ and .prototype

--- Section 29: Prototypes, Classes, & OOP/01_Prototypes/app.js
+++ Section 29: Prototypes, Classes, & OOP/01_Prototypes/app.js
@@ -3,6 +3,8 @@
 // __proto__ =  property that references the "datatype" prototype; the prototype includes all the methods available to the datatype.
 	// __proto__ is the one object that is a prototype for all instances of that type of object
 
+
+// __proto__ = reference to prototype; datatype.prototype = the actual prototype
 //String.prototype is a "template object" for every single string.
 //We could go crazy and add our own method called yell...
 String.prototype.yell = function() {
